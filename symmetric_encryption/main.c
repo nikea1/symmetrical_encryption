9d8a562d4ac75e1816bcd1cd151d743055edf4dd
Adenike Adeyemi <nike.adeyemi1@gmail.com>
2017-07-22 15:09:50 -0400

    Initial Commit

---
+++ symmetric_encryption/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  symmetric_encryption
+//
+//  Created by Adenike Adeyemi on 7/22/17.
+//  Copyright © 2017 Adenike Adeyemi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
