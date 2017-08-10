ecb5ffe64694f86af6cec3a60eb072664f12d928
Adenike Adeyemi <nike.adeyemi1@gmail.com>
2017-08-09 23:38:13 -0400

    Cleaned up code

--- symmetric_encryption/main.c
+++ symmetric_encryption/main.c
@@ -32,16 +32,17 @@ void printBin(int num, int nbits){
     
 }
 
+void printByte(char c){
+    printBin(c, 8);
+}
 
 int reverseByte(char c){
 
     char l,r;
     //masks
-    int m1 = 0xFF7F; //1111 1111 0111 1111
+
     int m2 = 0x0001; //0000 0000 0000 0001
     int m3 = 0x0080; //0000 0000 1000 0000
-    int m4 = 0xFFFE; //1111 1111 1111 1110
-    
     
     for(int j = 0; j < 4; j++){
         
@@ -49,23 +50,21 @@ int reverseByte(char c){
         l = (m3&c)>>(7-j);
         r = (m2&c)>>j;
         
-        //check if different
-        //if different flip bits
-        if(l != r){
+        //check for difference in left and right pointer bits
+        if(l != r){ //swap when difference is found
             if(l == 1){
-                c =((c&m1)|m2);
+                c =((c&(~m3))|m2);
             }
             else{
-                c = ((c|m3)&m4);
+                c = ((c|m3)&(~m2));
             }
         }
         //shift mask
-        m1 = (m1>>1);
+
         m2 = (m2<<1);
         m3 = (m3>>1);
-        m4 = (m4<<1)+1;
         
-    }
+    }//end of for loop
 
     return (0x00FF&c);
 }
@@ -101,6 +100,7 @@ int orderKey(char c, int key){
     
     return ((q*key)+r)&0x00FF;
 }
+
 //encoder
 char *encode(char message[], int key){
     if(key > 0x0F || key < 0x08){
@@ -137,100 +137,77 @@ char *decode(char cipher[], int key){
 
 int main(int argc, const char * argv[]) {
     
-//    char str[] = "Hello, World!";
-//    
-//    printf("%s\n", str);
-//    encode(str, 8);
-//    printf("%s\n", str);
-//    decode(str, 8);
-//    printf("%s\n", str);
-//
-//    printf("%d\n", argc);
-    
-    if(argc == 4){
-        int key = atoi(argv[3]);
-        if(key < 8 || key > 15){
+    if(argc == 4){                                // Make sure command is formatted correctly
+        
+        int key = atoi(argv[3]);                  //get 4-bit key
+        
+        if(key < 8 || key > 15){                  //make sure key is 4-bit else terminate
             printf("Error: Key is out of bounds\n");
             exit(1);
         }
         
-        FILE *fpi, *fpo;
-        int flag = 0;
+        FILE *fpi, *fpo;                          //initalize file pointers for input and output
         
-        fpi = fopen(argv[2], "r");
-        fpo = fopen("cipher.txt", "w+");
+        int flag = 0;                             //set flag for encode and decode success
         
-        if(fpi == NULL || fpo == NULL){
+        fpi = fopen(argv[2], "r");                //open text to be encoded or decoded for reading file
+        fpo = fopen("cipher.txt", "w+");          //open file to write output
+        
+        if(fpi == NULL || fpo == NULL){           //make sure files are opened else close files and exit
             printf("Error: file does not exist\n");
             fclose(fpi);
             fclose(fpo);
             exit(1);
         }
         
-       
-        int f_length = 2048;
-        fseek(fpi, 0, SEEK_SET);
+        int f_length = 2048;                      //buffer length
+        fseek(fpi, 0, SEEK_SET);                  //make sure pointer is at the beginning
         
-        char buffer[f_length];
+        char buffer[f_length];                    //create buffer
         
-        //if encoder mode
-        if(strcmp(argv[1], "encode") == 0){
-            //loop through file untill the end and hash the message
-            while(feof(fpi) == 0){
+        if(strcmp(argv[1], "encode") == 0){       //if encoder mode
+            
+            while(feof(fpi) == 0){                //loop through file until end of file and encode text
                 fgets(buffer, f_length, fpi);
                 encode(buffer, 8);
                 fputs(buffer, fpo);
             }
-            //flag for completion
-            flag = 1;
             
-        }
-        //if decoder mode
-        else if(strcmp(argv[1], "decode") == 0){
-            //loop through ciphered text untill the end and decode the message
-            while(feof(fpi) == 0){
+            flag = 1;                             //flag for completion
+            
+        }//end of if(encode)
+        
+        else if(strcmp(argv[1], "decode") == 0){  //if decoder mode
+            while(feof(fpi) == 0){                //loop through file until end of filr and decode cipher
+
                 fgets(buffer, f_length, fpi);
                 decode(buffer, 8);
                 fputs(buffer, fpo);
             }
-            //flag for completion
-            flag = 1;
-        }
-        else{//if something else other than decode or encode
+            
+            flag = 1;                             //flag for completion
+        }//end of else if(decode)
+        else{                                     //if something else other than decode or encode
             printf("Error: Invalid command. Has to be 'decode' or 'encode' without quotes\n");
             fclose(fpi);
             fclose(fpo);
             exit(1);
         }
         
-        //close files
-        fclose(fpi);
+        fclose(fpi);                              //close input and output files
         fclose(fpo);
         
-        //destroy old file and rename cipher to old file name
         if(flag == 1){
-            remove(argv[2]);
-            rename("cipher.txt", argv[2]);
+            remove(argv[2]);                      //destroy input file
+            rename("cipher.txt", argv[2]);        //rename output file to old file name
         }
         
-    }
+    }//end of if(argc == 4)
     else{
-        //print out correct format of command
-        printf("Format: %s [mode] [text file] [key]\n", argv[0]);
-    }//argc == 4
+        
+        printf("Format: %s [mode] [text file] [key]\n", argv[0]); //print out correct format of command
+    }//end of else
     
 
-//        FILE *fp;
-//        
-//        fp = fopen("test.txt", "w+");
-//        fprintf(fp, "This is testing for fprintf...\n");
-//        fputs("This is testing for fputs...\n", fp);
-//        fclose(fp);
-    
-//    FILE *fp;
-//    
-//    fp = fopen("test.txt", "r+");
-//    fclose(fp);
-    
     return 0;
 }
